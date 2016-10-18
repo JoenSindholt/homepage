4d7ad816fb677d9e7a887a31c88fa843b4944ce5
joensindholt <joensindholt@gmail.com>
2016-10-18 11:09:11 +0200

    restarting

--- Homepage/Models/ImageIndexViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Homepage.Models
-{
-    public class ImageIndexViewModel
-    {
-        public IEnumerable<ImageSectionViewModel> Sections { get; set; }
-    }
-}
--- Homepage/Models/ImageViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Homepage.Models
-{
-    public class ImageViewModel
-    {
-        public string Src { get; set; }
-
-        public string LargeSrc { get; set; }
-    }
-}
